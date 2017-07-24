86497f4c0352d0bfc5dc73fe857079bfd2460625
Vidsneezes <moraleszez@gmail.com>
2017-07-24 06:15:03 -0600

    added tools embeddedgist and welcome interface js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import WelcomeInterface from './WelcomeInterface';
+import EmbeddedGist from './Tools/EmbeddedGist';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <WelcomeInterface />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <EmbeddedGist gist = "Shininglow/56ee6effa33d711d46b651fc0dd09366" />
       </div>
     );
   }
---
+++ src/WelcomeInterface.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WelcomeInterface extends Component {
+  render() {
+    return (
+      <div>
+        <p>Oscar Morales says...</p>
+        <p>Hello Visitor</p>
+      </div>
+    );
+  }
+}
+
+export default WelcomeInterface;
