163710ed8664f09a2b08b08a9c64061790a78507
Vidsneezes <moraleszez@gmail.com>
2017-07-24 20:01:16 -0600

    changes from dm at to email me at

--- src/WelcomeInterface.js
+++ src/WelcomeInterface.js
@@ -6,7 +6,7 @@ class WelcomeInterface extends Component {
     return (
       <div className="WI-Head">
         <h1>Oscar Morales says...{this.props.page}</h1>
-        <h3>Hello Visitor, DM at moraleszez@gmail.com
+        <h3>Hello Visitor, Email me at moraleszez@gmail.com
          <a href="https://twitter.com/Moraleszez">@Moraleszez</a> {"   "}
         <a href="https://github.com/Vidsneezes">My Github</a></h3>
       </div>
